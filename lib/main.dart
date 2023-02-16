330cb13f82e2797a841587513ce14c552ea62aa5
Christian David Ospina Primero <christianprimero26@hotmail.com>
2023-02-16 00:59:19 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,19 @@ class MiApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: "Mi app",
-      home: Inicio(),
+      home: Home(),
     );
   }
 }
 
-class Inicio extends StatefulWidget {
-  const Inicio({super.key});
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
-  State<Inicio> createState() => _InicioState();
+  State<Home> createState() => _HomeState();
 }
 
-class _InicioState extends State<Inicio> {
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
